76f799dab69f1d603a526f12533e5c8e5542d2ad
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-08 23:00:46 -0400

    Added placeholder images for recipe photo uploads

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -16,4 +16,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
+
+  def default_url
+      ActionController::Base.helpers.asset_path("fallback/" + [version_name, "placeholder.jpg"].compact.join('_'))
+  end
 end
